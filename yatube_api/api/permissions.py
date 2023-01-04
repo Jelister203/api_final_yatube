88cabdf2781c722098ae78953f238b14c045184b
Jelister203 <93206051+Jelister203@users.noreply.github.com>
2023-01-04 14:38:04 +0300

    First iteration

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,14 +4,14 @@ from rest_framework import permissions
 class AuthorPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
-        request.method in permissions.SAFE_METHODS
-        or obj.author == request.user
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
         )
 
     def has_permission(self, request, view):
         return (
-        not request.user.is_anonymous
-        or request.method in permissions.SAFE_METHODS
+            not request.user.is_anonymous
+            or request.method in permissions.SAFE_METHODS
         )
 
 
